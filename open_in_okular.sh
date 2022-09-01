a4228e3d7173236fce7782f86a9ceabf911036da
Julian Barg <barg.julian@gmail.com>
2022-09-01 12:51:00 -0400

    Ensure that pdf is opened in existing window.

---
+++ open_in_okular.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Go to okular to ensure that pdf is opened in existing window.
+# Note: -R also works but turns off any flags like
+# "Always on Visible Workspace"
+
+if [[ $( wmctrl -l | grep "okular" ) ]]; then
+	wmctrl -a "okular"
+	wmctrl -R "okular"
+	sleep 0.3
+fi
+
+okular "$1"
