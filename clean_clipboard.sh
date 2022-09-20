fbe25df358722abe8ef654266b49d3ab967ea50e
Julian Barg <barg.julian@gmail.com>
2022-09-20 15:57:02 -0400

    Quick function to clean clipboard content.

---
+++ clean_clipboard.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Copy function for my coded data
+
+# Parses currently selected text
+
+xsel --clipboard \
+	| sed -e "s/|.*//g" -e 's/-$/||/g' -e 's/^\s*$/|/g' \
+    | tr '\n' ' ' \
+    | sed -e 's/|| //g' -e 's/| /\n\n/g' -e 's/|$//' \
+	| xsel -bi
