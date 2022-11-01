8e2ae2ca04366c8e92f2bfdf29445106743211a2
Julian Barg <barg.julian@gmail.com>
2022-11-01 15:08:18 -0400

    Improved on reflow function.

--- reflow_md.sh
+++ reflow_md.sh
@@ -2,21 +2,23 @@
 
 set -o nounset
 
-FILE=$1
+reflow () {
+    FILE=$1
 
-if [[ $(head -n 1 "$FILE") = "---" ]]; then
-    YAML_END=$(grep -m 2 -n "^---$" "$FILE" \
-    | cut -f1 -d: \
-    | tail -n 1)
-    HEAD=$(head -n $YAML_END "$FILE")
-    body=$(sed "1,${YAML_END}d" "$FILE")
-else
-    HEAD=""
-    body=$(cat "$FILE")
-fi
+    if [[ $(head -n 1 "$FILE") = "---" ]]; then
+        YAML_END=$(grep -m 2 -n -E "^---\s?$" "$FILE" \
+        | cut -f1 -d: \
+        | tail -n 1)
+        HEAD=$(head -n $YAML_END "$FILE")
+        body=$(sed "1,${YAML_END}d" "$FILE")
+        body=$( fmt <(echo "$body") -w 72 | sed "s/  / /g")
+        cat <(echo "$HEAD") <(echo "$body") > "$HOME/tmp/reflow.md"
+    else
+        body=$(cat "$FILE")
+        fmt <(echo "$body") -w 72 | sed "s/  / /g" \
+            > "$HOME/tmp/reflow.md"
+    fi
+    cp "$HOME/tmp/reflow.md" "$FILE"
+}
 
-body=$( fmt <(echo "$body") -w 72 )
-
-echo "$HEAD" "$body" > "$HOME/tmp/reflow.md"
-
-cp "$HOME/tmp/reflow.md" "$FILE"
+reflow $@
