5e3e8b051efb8be0d45700dd113989cc0c20626f
Julian Barg <barg.julian@gmail.com>
2023-08-11 17:13:36 -0400

    made latest a script file.

--- latest.sh
+++ latest.sh
@@ -1,40 +1,50 @@
-latest () {
-    local number
-    local recursive
-    POSITIONAL_ARGS=()
-    for i in "$@"; do
-        case $i in
-            -n)
-             number="$2"
-             shift
-             shift
-             ;;
-            # -r)
-            #  recursive="true"
-            #  shift
-            #  shift
-            #  ;;
-            *)
-             POSITIONAL_ARGS+=("$1")
-             shift
-             ;;
-        esac
-    done
-    set -- "${POSITIONAL_ARGS[@]}"
-    folder=$1
-    
-    if [[ -z $number ]]; then
-        number=1
-    fi
+#!/usr/bin/env bash
 
-    # if [[ "$recursive" = true ]]; then
-        # Makes it so it only searches non-hidden
-    files="$(find $folder -not -path '*/.*' -type f -printf '%T@ %p\n' \
-        | sort -n \
-        | tail -"$number" \
-        | cut -f2- -d" ")"
-    # else
-    #     ls $folder -t | head -n $number
-    # fi
-    echo "$files"
+# Default values
+number=1
+folder="."
+recursive=false
+
+print_help() {
+    echo "Usage: $0 [-n number_of_files] [-r|--recursive] [folder_location]"
+    echo "  -n: Number of latest files to fetch. Default is 1."
+    echo "  -r, --recursive: Search recursively in subfolders."
+    echo "  folder_location: Directory to search in. Default is current directory."
+    exit 1
 }
+
+# Check if no arguments provided
+if [ "$#" -eq 0 ]; then
+    print_help
+fi
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -n)
+            shift
+            number="$1"
+            ;;
+        -r|--recursive)
+            recursive=true
+            ;;
+        *)
+            # Assuming it's the folder location
+            folder="$1"
+            ;;
+    esac
+    shift
+done
+
+# Check if recursive flag is set
+if $recursive; then
+    find_cmd="find \"$folder\" -not -path '*/.*' -type f"
+else
+    find_cmd="find \"$folder\" -maxdepth 1 -not -path '*/.*' -type f"
+fi
+
+# Execute the command
+eval "$find_cmd -printf '%T@ %p\n'" \
+    | sort -n \
+    | tail -"$number" \
+    | cut -f2- -d" "
+
